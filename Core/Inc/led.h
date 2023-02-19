c05c31b6c3430812808db8479b9117d9821858a7
daest <steffen@daehmcke.de>
2023-02-19 20:53:50 +0100

    Added LED_OFF status

--- Core/Inc/led.h
+++ Core/Inc/led.h
@@ -10,9 +10,10 @@
 
 #include "stm32l0xx_hal.h"
 
-#define LED_R 0x01
-#define LED_G 0x02
-#define LED_B 0x03
+#define LED_OFF   0x00
+#define LED_R     0x01
+#define LED_G     0x02
+#define LED_B     0x03
 
 
 void led_init();
