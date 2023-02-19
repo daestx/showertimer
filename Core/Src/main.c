ebfdb960b6c0310ea3dcfd6d6462c86eec0af98b
daest <steffen@daehmcke.de>
2023-02-19 21:04:43 +0100

    added missing includes

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,7 +18,9 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-
+#include "adc.h"
+#include "dma.h"
+#include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -85,7 +87,9 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
-
+  MX_GPIO_Init();
+  MX_DMA_Init();
+  MX_ADC_Init();
   /* USER CODE BEGIN 2 */
   /* USER CODE END 2 */
 
