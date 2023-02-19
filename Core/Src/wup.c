d18c41884f2092868c00894ef9d1f9b253ab2610
daest <steffen@daehmcke.de>
2023-02-19 21:02:55 +0100

    clear ISR flag for EXTI

--- Core/Src/wup.c
+++ Core/Src/wup.c
@@ -8,23 +8,25 @@
 #include "wup.h"
 #include "led.h"
 
-// activate external interrupt source
-void wup_activate(){
 
-}
+// clear external interrupt flag
+void wup_clearIsrFlags(){
 
-
-// suspend external interrupt source
-void wup_suspend(){
+   __HAL_GPIO_EXTI_CLEAR_FLAG(GPIO_PIN_14);
+   NVIC_ClearPendingIRQ(EXTI4_15_IRQn);
 
 }
 
 
 void wup_sleep(){
+   // clear pending interrupt flags before switching to sleep mode.
+   // Otherwise system will wakeup immediately
+   wup_clearIsrFlags();
    // Put controller into stop mode and configure it to wait for interrupt.
    // Any external interrupt will wakeup the microcontroller. For this application
    // interrupt source connected to Hall sensor. No ISR required
    HAL_PWR_EnterSTOPMode(PWR_LOWPOWERREGULATOR_ON, PWR_STOPENTRY_WFE);
+
 
 
 }
