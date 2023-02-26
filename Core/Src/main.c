ff032b804bc067564c31c746fc83f557ba3fa948
daest <steffen@daehmcke.de>
2023-02-26 09:43:26 +0100

    Moved HAL init to modes & states

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -87,9 +87,6 @@ int main(void)
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_DMA_Init();
-  MX_ADC_Init();
   /* USER CODE BEGIN 2 */
   /* USER CODE END 2 */
 
